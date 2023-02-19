40369b0ae89ee93ed1a0742ccddc8399f5d7aaf5
WhiteMorro1177 <jackforest228@gmail.com>
2023-02-19 15:30:48 +0300

    test layouts + contact screen + orientation changes

---
+++ orientationchanges/src/main/java/ru/mirea/tsybulko/orientationchanges/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mirea.tsybulko.orientationchanges
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
