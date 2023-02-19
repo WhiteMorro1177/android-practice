a267bf803b7d4d4514017f779c1eabef6b51bf30
WhiteMorro1177 <jackforest228@gmail.com>
2023-02-19 16:13:19 +0300

    test layouts + contact screen(rework) + orientation changes

--- contactscreen/src/main/java/ru/mirea/tsybulko/contactscreen/MainActivity.kt
+++ contactscreen/src/main/java/ru/mirea/tsybulko/contactscreen/MainActivity.kt
@@ -1,11 +1,13 @@
 package ru.mirea.tsybulko.contactscreen
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linear_layout)
+        setContentView(R.layout.table_layout)
     }
 }
